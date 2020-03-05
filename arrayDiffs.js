21996fe5b0066dcfc1952212146f4054b9e0c0a2
donnywarren <51206190+donnywarren@users.noreply.github.com>
2020-03-04 20:37:37 -0500

    finished

--- arrayDiffs.js
+++ arrayDiffs.js
@@ -1,7 +1,10 @@
 //ARRAY DIFFERENCES
 
 const arrDifferences = (arr1, arr2) => {
-
+  return arr1.concat(arr2).filter(function (number) {
+    if (!(arr1.includes(number) && arr2.includes(number)))
+      return number;
+  });
 }
 
 console.log(arrDifferences([1, 44, 2, 3, 5], [33, 1, 2, 3, 4, 5]))
@@ -9,3 +12,6 @@ console.log(arrDifferences([1, 44, 2, 3, 5], [33, 1, 2, 3, 4, 5]))
 module.exports = {
   arrDifferences
 }
+
+
+
